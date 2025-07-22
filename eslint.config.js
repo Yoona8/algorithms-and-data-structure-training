98c31eae38692bb65f939a39a9241481990a1e1c
Yoona8 <elena243v@gmail.com>
2025-07-22 08:04:34 +0200

    feat: Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default defineConfig([
   {
     files: ['**/*.mjs'],
+    languageOptions: {
+      globals: globals.browser,
+    },
     plugins: { js },
     extends: ['js/recommended'],
     rules: {
